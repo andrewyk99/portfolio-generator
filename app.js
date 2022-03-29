1c98e118c4ecfc30149b7c512a5124d054ce1c9a
andrewyk99 <andrewyk99@hotmail.com>
2022-03-29 15:45:58 -0600

    generate html file and modularize code

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+const pageHTML = generatePage(name, github);
 
-// Notice the lack of parentheses around the 'prfoileDataArr' parameter
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
