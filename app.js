2ced791fecf565dc731780879a48d2ca0b8646fe
andrewyk99 <andrewyk99@hotmail.com>
2022-03-31 21:39:37 -0600

    input stylesheet, wrote copy and write file using promises

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile} = require('./utils/generate-site');
 const generatePage = require('./src/page-template');
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
 
 const promptUser = () => {
     return inquirer
@@ -148,12 +141,19 @@ const promptProject = portfolioData => {
     
 promptUser()
     .then(promptProject)
-    .then(projectAnswers => {
-        const pageHTML = generatePage(projectAnswers);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log('Page created! Checkout index.html in this directory to see it!');
-        });
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
